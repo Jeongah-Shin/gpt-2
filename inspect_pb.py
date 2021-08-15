5d156550f16ca5c1f6071bbb0a58b78472928ffc
root <111.15@sk.com>
2021-08-15 19:08:08 +0900

    Add utils for converting model graphs

---
+++ inspect_pb.py
@@ -0,0 +1,20 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+def printTensors(pb_file):
+
+    # read pb into graph_def
+    with tf.gfile.GFile(pb_file, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read(-1))
+
+    # import graph_def
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def)
+
+    # print operations
+    for op in graph.get_operations():
+        print(op.name)
+
+
+printTensors("./export_dir/117M/saved_model.pb")
